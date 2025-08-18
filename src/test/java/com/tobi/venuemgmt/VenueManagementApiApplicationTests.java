33d35d14332d179a3cf1a8130b60c960bd6a91b6
Tobijimoh <tobijimohh@gmail.com>
2025-08-18 14:16:47 +0100

    Initial commit - Spring Boot Venue Management API

---
+++ src/test/java/com/tobi/venuemgmt/VenueManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobi.venuemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenueManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
