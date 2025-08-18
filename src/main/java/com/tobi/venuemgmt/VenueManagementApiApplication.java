d31c3c0b21c31337c4490da0c34baae00d4a1ce9
Tobijimoh <tobijimohh@gmail.com>
2025-08-18 14:16:47 +0100

    Initial commit - Spring Boot Venue Management API

---
+++ src/main/java/com/tobi/venuemgmt/VenueManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.tobi.venuemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenueManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenueManagementApiApplication.class, args);
+	}
+
+}
